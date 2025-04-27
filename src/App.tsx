279d246be4a454946546f87b6198c3d2282f3bca
isinaveira <isinaveira@gmail.com>
2025-04-27 20:02:15 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+  const [search, setSearch] = useState('');
+
+  return (
+    <>
+      <input type="text" />
+    </>
+  )
+}
+
+export default App
